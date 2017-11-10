cd3539b763efbc0a7945ae8ca649518364015207
hoanghieukhtn <hoanghieuhcmus@gmail.com>
2017-11-10 07:36:26 +0700

    update file food.jsp and foodControllers

--- src/main/java/com/jds/jds/customerapp/Controller/FoodsController.java
+++ src/main/java/com/jds/jds/customerapp/Controller/FoodsController.java
@@ -1,15 +1,30 @@
 package com.jds.jds.customerapp.Controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.jds.jds.customerapp.Dao.FoodDAO;
+import com.jds.jds.customerapp.Model.Food;
 
 
 @Controller
 public class FoodsController {
+	
+	@Autowired
+	private FoodDAO foodDAO;
 	@RequestMapping(value = "/Foods", method = RequestMethod.GET)
-    public ModelAndView foods(){
-        return new ModelAndView("foods");
+    public ModelAndView foods(@RequestParam("deptId") int departmentId, @RequestParam("catId") int categoryId,
+    		@RequestParam("page") int page){
+		ModelAndView mav = new ModelAndView("foods");
+		Map<Food, Integer> listFoods = (Map<Food, Integer>) this.foodDAO.findByDepartmentIdAndCategoryId(departmentId, categoryId, page, "foodName");
+		mav.addObject("listFoods", listFoods);
+		mav.addObject("deptId", departmentId);
+        return mav;
     }
 }
