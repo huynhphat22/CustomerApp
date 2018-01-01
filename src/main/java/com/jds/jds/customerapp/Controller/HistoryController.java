251bc8b43b60b4c8da6b4e0661da6cb45d52d35f
Võ Duy Thảo <duythao56@gmail.com>
2018-01-01 23:41:07 +0700

    change password for customer

--- src/main/java/com/jds/jds/customerapp/Controller/HistoryController.java
+++ src/main/java/com/jds/jds/customerapp/Controller/HistoryController.java
@@ -1,10 +1,12 @@
 package com.jds.jds.customerapp.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -59,6 +61,57 @@ public class HistoryController {
 		mav.addObject("list", list);
 		mav.addObject("count", count);
 		mav.addObject("customer", customer);
+		return mav;
+	}
+	
+	@RequestMapping(value = "/History", method = RequestMethod.POST)
+	public ModelAndView changePassword(HttpServletRequest request, @RequestParam("page") int page, 
+			@RequestParam(value = "orderId", required=false) String oid,
+			@RequestParam("password") String password, @RequestParam("newPassword") String newPassword,
+			@RequestParam("reNewPassword") String reNewPassword) {
+		Customer customer = (Customer) request.getSession().getAttribute("customer");
+		if(customer == null){
+			return new ModelAndView("redirect:/Login");
+		}
+		ModelAndView mav = new ModelAndView("history");
+		List<OrderDetail> listDetails = null;
+		
+		if(oid != null){
+			int orderId = 0;
+			try{
+				orderId = Integer.parseInt(oid);
+				listDetails = (List<OrderDetail>) this.orderDetailDAO.findByCustomerIdAndOrderId(orderId, customer.getCustomerId());
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+		customer = this.customerDAO.findById(customer.getCustomerId());
+		List<OrderFood> list = (List<OrderFood>) this.orderFoodDAO.paginateOrderFoodByCustomerId(page, "orderId", customer.getCustomerId());
+		long count = this.orderFoodDAO.countByCustomerId(customer.getCustomerId());
+		mav.addObject("listDetails", listDetails);
+		mav.addObject("list", list);
+		mav.addObject("count", count);
+		mav.addObject("customer", customer);
+		
+		
+		
+		if(!newPassword.equals(reNewPassword) || newPassword.length() < 6){
+			mav.addObject("message", "Invalid New Password Or Confirm Password Is Incorrect!");
+			return mav;
+		}
+		
+		BCryptPasswordEncoder bpe = new BCryptPasswordEncoder();
+    	if(!bpe.matches(password, customer.getPassword())){
+    		mav.addObject("message", "Password is incorrect!");
+			return mav;
+    	}
+    	
+		customer.setPassword(bpe.encode(newPassword));
+		
+		this.customerDAO.update(customer);
+		mav.addObject("message", "Change Password successfully");
 		return mav;
 	}
 }
